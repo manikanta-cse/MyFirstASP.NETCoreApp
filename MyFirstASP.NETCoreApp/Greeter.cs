9a496171b3477561491a0119686d864fbabdeb06
Kattamuri, Manikanta <Manikanta.Kattamuri@cdk.com>
2017-06-19 11:12:51 +0530

    middleware setup

---
+++ MyFirstASP.NETCoreApp/Greeter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+
+namespace MyFirstASP.NETCoreApp
+{
+    public class Greeter : IGreeter
+    {
+        private string _greeting;
+
+        public Greeter(IConfiguration configuration)
+        {
+           _greeting= configuration["Greeting"];
+        }
+        public string GetGreeting()
+        {
+            return _greeting;
+        }
+    }
+}
--- MyFirstASP.NETCoreApp/Startup.cs
+++ MyFirstASP.NETCoreApp/Startup.cs
@@ -1,10 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
@@ -12,14 +10,29 @@ namespace MyFirstASP.NETCoreApp
 {
     public class Startup
     {
+        public Startup(IHostingEnvironment env)
+        {
+            var builder= new ConfigurationBuilder()
+                                .SetBasePath(env.ContentRootPath)
+                                .AddJsonFile("appsettings.json")
+                                .AddEnvironmentVariables();
+
+            Configuration= builder.Build();
+        }
+
+        public IConfiguration Configuration { get; set; }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvcCore();
+            services.AddSingleton(Configuration);
+            services.AddSingleton<IGreeter, Greeter>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory,IGreeter greeter)
         {
             loggerFactory.AddConsole();
 
@@ -27,11 +40,32 @@ namespace MyFirstASP.NETCoreApp
             {
                 app.UseDeveloperExceptionPage();
             }
-
-            app.Run(async (context) =>
+            else
             {
-                await context.Response.WriteAsync("Hello World!");
-            });
+                app.UseExceptionHandler(new ExceptionHandlerOptions()
+                {
+                    ExceptionHandler = context=>context.Response.WriteAsync("Oops!")
+                });
+            }
+
+            //app.UseDefaultFiles();
+            //app.UseStaticFiles();
+
+            app.UseFileServer(); // server the static files nad default files too
+
+            //app.UseWelcomePage(new WelcomePageOptions()
+            //{
+            //    Path = "/welcome"
+            //});
+
+            //app.Run(async (context) =>
+            //{
+            //    //throw  new Exception("Error");
+            //    var message = greeter.GetGreeting();
+            //    await context.Response.WriteAsync(message);
+            //});
+
+            app.UseMvcWithDefaultRoute();
         }
     }
 }
